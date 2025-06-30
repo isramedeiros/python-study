7f8618a26c8683ec256e28fb82d9bc08a44b9324
Israel Medeiros <israelmfreitas12@gmail.com>
2025-06-30 11:07:29 -0300

    initial commit

---
+++ counter.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+list = ["a", "b", "a", "c", "a", "b", "l", "l", "m"]
+counter = Counter(list)
+
+print(counter)
+
+#len() returns the length of the iterable
+print(len(list))
