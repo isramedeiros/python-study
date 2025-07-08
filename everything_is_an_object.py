afc965652c60da4086d5274e1b2106e79170d711
Israel Medeiros <israelmfreitas12@gmail.com>
2025-07-08 13:16:03 -0300

    Added everything is an object

---
+++ everything_is_an_object.py
@@ -0,0 +1,4 @@
+print(type(10))           # <class 'int'>
+print(type("hello"))      # <class 'str'>
+print(type([1, 2, 3]))    # <class 'list'>
+print(type(len))          # <class 'builtin_function_or_method'>
